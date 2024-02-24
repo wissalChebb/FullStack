b780a1a5e1190cc39eb435b074f377d3137e6e94
wissalChebb <98927814+wissalChebb@users.noreply.github.com>
2024-02-24 16:50:49 +0100

    new wissal

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/',(req, res) =>{
-    res.send({hi : 'wissal'});
+    res.send({hi : 'ahmed'});
 });
 const PORT =process.env.PORT || 3000;
 app.listen(PORT)
